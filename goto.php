70112420a28a98d44f4a4c254bb58774e96db64a
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2022-06-07 22:10:28 +0700

    membuat file goto.php

---
+++ goto.php
@@ -0,0 +1,9 @@
+<?php
+// goto itu pergi ke line program yang udah dilabelin, emang katanya bikin pusin sih. tapi seru bisa bikin perulangan waaw
+b:
+echo 'di skip moal yeuh?';
+goto a;
+
+a:
+echo 'Ini go to a';
+goto b;
