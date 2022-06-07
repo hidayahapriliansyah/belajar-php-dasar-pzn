e85e36fcf0b6840fdffee69fca7c2cdcb340974a
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2022-06-07 19:48:49 +0700

    buat file if dan switch

---
+++ ifstatement.php
@@ -0,0 +1,12 @@
+<?php
+
+$nilai = 100;
+if ($nilai == 100) {
+    echo 'nilai kamu perfecto maknyuuuussss';
+} elseif ($nilai > 90) {
+    echo 'kamu adalah seorang ekspert';
+} elseif ($nilai >= 80 && $nilai < 90) {
+    echo 'ayo sedikit lagi tingkatkan yuk bisa yuk';
+} else {
+    echo 'ayo tingkatkan lagi';
+}
---
+++ switchstatement.php
@@ -0,0 +1,15 @@
+<?php
+
+$nilai = 'C';
+
+switch ($nilai) {
+    case 'A':
+        echo 'Nilai kamu A';
+        break;
+    case 'B':
+        echo "Nilai kamu B";
+        break;
+    default:
+        echo 'JELEK BANGET LU PUNYA NILAI MAMANG';
+        break;
+}
