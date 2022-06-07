c7b752cf88138b00f173faaeeba351458c69874f
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2022-06-07 21:26:51 +0700

    megubah break_continue.php

--- break_continue.php
+++ break_continue.php
@@ -1,5 +1,6 @@
 <?php
 
+//  break itu untuk menghentikan perulangan seluruhnya(kode yang di bawahnya)
 $counter = 1;
 while (true) {
     echo "Ini perulangan ke-$counter" . PHP_EOL;
@@ -7,4 +8,25 @@ while (true) {
     if ($counter == 11) {
         break;
     }
+}
+
+$nama = "Adi Muhamad Firmansyah";
+$banyakKarakter = strlen($nama);
+
+$counter = 0;
+while (true) {
+    echo $nama[$counter] . PHP_EOL;
+    $counter++;
+    if ($counter == $banyakKarakter) {
+        break;
+    }
+}
+
+// continue akan melanjutkan periungan dengan menskip kode di bawahnya; menghentikan perulangan saat ini dan melanjutkan perulangan selanjutnya;
+
+for ($counter = 1; $counter <= 10; $counter++) {
+    if ($counter % 2 == 0) {
+        continue;
+    }
+    echo "Ini perulangan ke-$counter" . PHP_EOL;
 }
